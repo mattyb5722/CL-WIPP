3f15a8687f1ab2c35c32fe3268e11855be10c1d5
Matthew Brown <brownm18@rpi.edu>
2019-04-05 16:44:22 -0400

    Added Mapbox

---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,17 @@
+﻿using Mapbox.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MapboxAccess.Instance.ClearAllCacheFiles();
+    }
+    private void Update() {
+        if (Input.GetKey("W")) {
+            print("W");
+        }
+    }
+}
